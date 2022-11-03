897d9d2c13baaf16e17036ca0477edc458fbbaf7
Faded-User <wolkdem@gmail.com>
2022-11-03 22:39:08 +0200

    done. No need to corect work. Maybe visual effect needs some improvements

---
+++ js/scriptSounds.js
@@ -0,0 +1,175 @@
+// const buttons = [...document.querySelectorAll(".sound")];
+// var audio = new Audio(.../);
+// // 0-16
+// //
+// var Audio0 = document.getElementById("audio0");
+// var Audio1 = document.getElementById("audio1");
+// var Audio2 = document.getElementById("audio2");
+// var Audio3 = document.getElementById("audio3");
+// var Audio4 = document.getElementById("audio4");
+// var Audio5 = document.getElementById("audio5");
+// var Audio6 = document.getElementById("audio6");
+// var Audio7 = document.getElementById("audio7");
+// var Audio8 = document.getElementById("audio8");
+// var Audio9 = document.getElementById("audio9");
+// var Audio10 = document.getElementById("audio10");
+// var Audio11 = document.getElementById("audio11");
+// var Audio12 = document.getElementById("audio12");
+// var Audio13 = document.getElementById("audio13");
+// var Audio14 = document.getElementById("audio14");
+// var Audio15 = document.getElementById("audio15");
+// var Audio16 = document.getElementById("audio16");
+//
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+
+// buttons[0].onclick = function () {
+//   Audio0.play();
+// };
+// buttons[1].onclick = function () {
+//   Audio1.play();
+// };
+// buttons[2].onclick = function () {
+//   Audio2.play();
+// };
+// buttons[3].onclick = function () {
+//   Audio3.play();
+// };
+// buttons[4].onclick = function () {
+//   Audio4.play();
+// };
+// buttons[5].onclick = function () {
+//   Audio5.play();
+// };
+// buttons[6].onclick = function () {
+//   Audio6.play();
+// };
+// buttons[7].onclick = function () {
+//   Audio7.play();
+// };
+// buttons[8].onclick = function () {
+//   Audio8.play();
+// };
+// buttons[9].onclick = function () {
+//   Audio9.play();
+// };
+// buttons[10].onclick = function () {
+//   Audio10.play();
+// };
+// buttons[11].onclick = function () {
+//   Audio11.play();
+// };
+// buttons[12].onclick = function () {
+//   Audio12.play();
+// };
+// buttons[13].onclick = function () {
+//   Audio13.play();
+// };
+// buttons[14].onclick = function () {
+//   Audio14.play();
+// };
+// buttons[15].onclick = function () {
+//   Audio14.play();
+// };
+// buttons[16].onclick = function () {
+//   Audio16.play();
+// };
+
+
+// New script sound for buttons
+
+function zeroClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/0.mp3'; 
+    audio.autoplay = true; 
+}
+
+function oneClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/1.mp3'; 
+    audio.autoplay = true; 
+}
+function twoClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/2.mp3'; 
+    audio.autoplay = true; 
+}
+function threeClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/3.mp3'; 
+    audio.autoplay = true; 
+}
+function fourClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/4.mp3'; 
+    audio.autoplay = true; 
+}
+function fiveClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/5.mp3'; 
+    audio.autoplay = true; 
+}
+function sixClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/6.mp3'; 
+    audio.autoplay = true; 
+}
+function sevenClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/7.mp3'; 
+    audio.autoplay = true; 
+}
+function eightClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/8.mp3'; 
+    audio.autoplay = true; 
+}
+function nineClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/9.mp3'; 
+    audio.autoplay = true; 
+}
+
+function clearClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/clear.mp3'; 
+    audio.autoplay = true; 
+}
+
+function divideClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/divide.mp3'; 
+    audio.autoplay = true; 
+}
+
+function dotClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/dot.mp3'; 
+    audio.autoplay = true; 
+}
+
+function minusClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/minus.mp3'; 
+    audio.autoplay = true; 
+}
+
+function multiplyClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/multiply.mp3'; 
+    audio.autoplay = true; 
+}
+
+function plusClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/plus.mp3'; 
+    audio.autoplay = true; 
+}
+
+function resultClick() {
+    var audio = new Audio(); 
+    audio.src = '/music/result.mp3'; 
+    audio.autoplay = true; 
+}
